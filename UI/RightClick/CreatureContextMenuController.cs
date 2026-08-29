5597b5511536f5a56266f6ad7a131c4268b193bd
Paulina Grey <155269966+pgreybyrd@users.noreply.github.com>
2026-08-28 21:08:37 -0700

    Add creature context menu controller

---
+++ UI/RightClick/CreatureContextMenuController.cs
@@ -0,0 +1,16 @@
+﻿using System.Windows;
+
+namespace Desktop_Creatures.UI.RightClick;
+
+public sealed class CreatureContextMenuController
+{
+    private CreatureContextMenuWindow? _window;
+
+    public bool IsOpen =>
+        _window is not null;
+
+    public void Close()
+    {
+        _window?.Close();
+    }
+}
