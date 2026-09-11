15a78461ddbc9b12e01ecf5388b21a265a13e325
Paulina Grey <155269966+pgreybyrd@users.noreply.github.com>
2026-09-11 14:41:56 -0700

    Simplify creature factory for data-driven species

--- Creatures/CreatureFactory.cs
+++ Creatures/CreatureFactory.cs
@@ -13,53 +13,10 @@ public static class CreatureFactory
         CreatureSettings settings,
         CreatureServices services)
     {
-        return definition.Id.ToLowerInvariant() switch
-        {
-            "rat" =>
-                new DataDrivenCreature(
-                    definition,
-                    context,
-                    settings,
-                    services),
-
-            "squirrel" =>
-                new DataDrivenCreature(
-                    definition,
-                    context,
-                    settings,
-                    services),
-
-            "skunk" =>
-                new DataDrivenCreature(
-                    definition,
-                    context,
-                    settings,
-                    services),
-
-            "hummingbird" =>
-                new DataDrivenCreature(
-                    definition,
-                    context,
-                    settings,
-                    services),
-
-            "eagle" =>
-                new DataDrivenCreature(
-                    definition,
-                    context,
-                    settings,
-                    services),
-
-            "ocelot" =>
-                new DataDrivenCreature(
-                    definition,
-                    context,
-                    settings,
-                    services),
-
-            _ =>
-                throw new NotSupportedException(
-                    $"Creature type '{definition.Id}' is not supported.")
-        };
+        return new DataDrivenCreature(
+            definition,
+            context,
+            settings,
+            services);
     }
 }
